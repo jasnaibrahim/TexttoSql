f29ac218b3ed0a8afc962614335e221615abad9b
jasnaibrahim <121844534+jasnaibrahim@users.noreply.github.com>
2024-11-06 12:20:01 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from langchain_community.utilities import SQLDatabase
 from psycopg2 import OperationalError  # For error handling with PostgreSQL
 import streamlit as st
 
+@st.cache_resource(ttl="1h")  # Cache for 1 hour (3600 seconds)
 def create_database_connection():
     """
     Establish a connection to the PostgreSQL database using SQLAlchemy and LangChain's SQLDatabase utility.
@@ -11,11 +12,11 @@ def create_database_connection():
         SQLDatabase: A connection to the database if successful; otherwise, raises an OperationalError.
     """
     try:
-        # Fetch the database URL from Streamlit secrets
-        database_uri = st.secrets["DATABASE_URL"]
+        # Fetch the database URI from Streamlit secrets
+        database_uri = st.secrets["DATABASE_URI"]
         
         # Create the SQLAlchemy engine and establish a connection
         db = SQLDatabase(create_engine(database_uri))
         return db
-    except OperationalError as e:
+    except OperationalError:
         raise OperationalError("Database connection failed. Please check your credentials or network connection.")
